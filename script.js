e36b45109bea602bbc5988c6834c096c389e29e9
Renz Gayacao <renzkinggayacaowork@gmail.com>
2026-08-03 23:44:06 -0700

    Updated nav menu

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
 const navClose = document.querySelector('.nav-close');
+const overlay = document.querySelector('.overlay')
+const body = document.body;
 
 
-navToggle.addEventListener('click', ()=>{
-const isOpen = navLinks.classList.toggle('is-open'); navToggle.setAttribute('aria-expanded', isOpen);
-});
-
-navLinks.querySelectorAll('a').forEach(link => {navLinks.classList.remove('is-open');
-    navToggle.setAttribute('aria-expanded', 'false');
-});
 
 function openMenu(){
 navLinks.classList.add('is-open');
 navToggle.setAttribute('aria-expanded','true');
-document.body.style.overflow = 'hidden';
+body.classList.add('overlay');
 }
 
 function closeMenu(){
     navLinks.classList.remove('is-open');
     navToggle.setAttribute('aria-expanded','false');
     document.body.style.overflow = '';
+    body.classList.remove('overlay')
 }
 
 navToggle.addEventListener('click', openMenu);
@@ -30,6 +26,11 @@ navLinks.querySelectorAll('a').forEach(link => {
     link.addEventListener('click', closeMenu);
 });
 
-document.addEventLister('keydown', (e) => {
+document.addEventListener('keydown', (e) => {
     if(e.key === 'Escape') closeMenu();
 });
+
+
+document.querySelectorAll('*').forEach(el => {
+    if (el.offsetWidth > document.documentElement.clientWidth) console.log(el.offsetWidth, el);
+  });
