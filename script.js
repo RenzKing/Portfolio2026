f9346d801f53e2bdab7e75b20e0c5856241e0cb9
RenzKing <renzkinggayacaowork@gmail.com>
2026-09-06 10:46:38 -0700

    Updated hero

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ document.querySelectorAll('*').forEach(el => {
 
  switchBtn.addEventListener('click', ()=>{
    const isLight = document.body.classList.toggle('light');
-   heroImg.src = isLight ? 'images/portrait-light.jpg' : 'images/portrait-dark.jpg';
+   heroImg.src = isLight ? 'images/portrait-light.jpg' : 'images/2018 pic.jpg';
 
    switchBtn.textContent = isLight ? '☾' : '☀';
    switchBtn.setAttribute('aria-label', isLight ? 'Switch to dark mode' : 'Switch to light mode');
